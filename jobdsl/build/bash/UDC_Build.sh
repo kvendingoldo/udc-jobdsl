706147c4ac5136bc2ae70aa07baccc5f92944202
Alexander Sharov <kvendingoldo@yandex.ru>
2018-09-29 01:44:20 +0400

    Update build script

--- jobdsl/build/bash/UDC_Build.sh
+++ jobdsl/build/bash/UDC_Build.sh
@@ -1,10 +1,10 @@
 set -x
 
-MVN_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+MVN_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 TIMESTAMP=$(date "+%Y%m%d.%H%M%S")
 VERSION="${MVN_VERSION}-${TIMESTAMP}-${BUILD_NUMBER}"
 VERSION=$(sed 's/-SNAPSHOT//g' <<< ${VERSION})
 
 mvn versions:set -DnewVersion="${VERSION}"
 
-gcloud docker -a
+#gcloud docker -a
