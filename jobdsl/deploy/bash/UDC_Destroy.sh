0570924905b9d4ba4d88919576ffa66191db5a30
Alexander Sharov <kvendingoldo@yandex.ru>
2018-10-15 22:38:20 +0400

    Fix jobdsl/deploy/bash/UDC_Destroy.sh

--- jobdsl/deploy/bash/UDC_Destroy.sh
+++ jobdsl/deploy/bash/UDC_Destroy.sh
@@ -6,6 +6,6 @@ helm del --purge "${RELEASE_NAME}" || echo '[ERROR] Release does not exist'
 
 kubectl config set-context $(kubectl config current-context) --namespace=default
 
-kubectl delete namespace "${RELEASE_NAME}"
+kubectl delete namespace "${RELEASE_NAME}" || echo '[ERROR] Namespace does not exist'
 
-sleep 10
+sleep 25
